71451c1fa8ac371589d223a8bdd469997006f51f
keerthana <keerthanasrajapure@gmail.com>
2025-03-11 11:57:19 +0530

    Changes made to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I made changes to this file\n";
+  cout << "I made changes to this file!\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
